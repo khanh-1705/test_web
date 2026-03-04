cf49588216cd874862d26e6185770a454e6e5422
khanh-1705 <140408928+khanh-1705@users.noreply.github.com>
2026-03-04 18:56:18 -0400

    Home test Implementation

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css'
+
+export default function Home() {
+    return(
+       <div id="homePage">
+         <div id="home">
+            <h1 id="homeHeader">
+                I am Mia.
+            </h1>
+        </div>
+       </div>
+    )
+}
